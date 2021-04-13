80dbf66a09fd480427189d55ec682d25075b4165
SeonJungKim <ksj083895@gmail.com>
2021-04-13 19:43:29 +0500

    Initial Commit

---
+++ SwiftUI_DarkMode_TUT/SwiftUI_DarkMode_TUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DarkMode_TUTApp.swift
+//  SwiftUI_DarkMode_TUT
+//
+//  Created by 김선중 on 2021/04/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DarkMode_TUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
